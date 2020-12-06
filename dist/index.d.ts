53435bdccab3c45aa0176699686711afb950b7f3
allx <allx@mail.ru>
2020-12-06 12:37:00 +0200

    remove d.ts from dist and add to gitignore

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'sunrise-sunset-js' {
-  export function getSunrise(latitude: number, longitude: number, date?: Date): Date;
-  export function getSunset(latitude: number, longitude: number, date?: Date): Date;
-}
