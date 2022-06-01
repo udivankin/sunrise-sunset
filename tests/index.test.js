9f21e8e349f2d626278c0c4345250708b8083c13
Broda Noel <brodanoel@gmail.com>
2022-06-01 20:01:39 -0300

    Add breaking tests for places where there is no runrise

--- tests/index.test.js
+++ tests/index.test.js
@@ -11,6 +11,12 @@ describe('SunriseSunsetJS library', () => {
         .toEqual(new Date("Fri, 21 Jan 2000 07:59:37.362 GMT"))
   });
 
+  it('should return correct sunrise time in Reine, Norway (High Latitude)', () => {
+    // https://sunrise.maplogs.com/reine_norway.7078.html
+    expect(getSunrise(67.9323866, 13.0887329, new Date("2022-06-01T22:53:26.363Z")))
+        .toEqual(new Date("Mon, 17 Jul 2022 23:44:00.000 GMT"))
+  });
+
   it('should return correct sunset time for GMT', () => {
     expect(getSunset(51.1788, -1.8262, new Date("2000-01-21 12:00:00 GMT")))
         .toEqual(new Date("Fri, 21 Jan 2000 16:38:21.883 GMT"))
